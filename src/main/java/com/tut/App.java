00b952fb9330fddd66f465884fa7885d19c60094
MateChaitanya <chaitanyamate02@gmail.com>
2024-07-12 23:30:41 +0530

    Initial commit

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -1,5 +1,9 @@
  package com.tut;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,7 +15,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws IOException
     {
         System.out.println( "Hello World!" );
         
@@ -28,6 +32,20 @@ public class App
         st.setCity("Pune");
         System.out.println(st);
         
+        //Creating object of  address class 
+        Address ad = new Address();
+        ad.setStreet("Street1");
+        ad.setCity("DELHI");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date(0));
+        ad.setX(1.234);
+        
+        //Reading image
+        FileInputStream fis = new FileInputStream("src/main/java/pic.jpeg");
+        byte[] data = new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
+        
         // Open a new session
         Session session = factory.openSession();
         
@@ -37,11 +55,14 @@ public class App
         // Save the student object
         session.save(st);
         
+        session.save(ad);
+        
         // Commit the transaction
         tx.commit();
         
         // Close the session
         session.close();
+        System.out.println("DONE...");
         
         // Optional: Check if the factory is closed
 //        System.out.println(factory);
